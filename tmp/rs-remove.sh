49d8d30c2400f64dd0e02e3f49d1a7ac65b748fe
Bogdan <zbohdan7@gmail.com>
2021-10-03 11:07:01 +0000

    Removed tmp and modified

--- tmp/rs-remove.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-kubectl delete $(kubectl get all -n $1 | grep replicaset.apps | grep "0         0         0" | cut -d' ' -f 1) -n $1
-
--- tmp/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-result=$(kubectl get all -l app=payment-processor)
-
-if [ -z "$result" ]; then
-  echo "Empty"
-else
-  kubectl rollout restart deploy payment-processor
-fi
